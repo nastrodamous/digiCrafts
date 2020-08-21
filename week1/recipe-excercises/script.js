a8fc137f3f47005c7ee379b9e3368051ad98bc52
nastrodamous <andre@andrejdavis.com>
2020-08-20 20:23:16 -0400

    Excercise 1

---
+++ week1/recipe-excercises/script.js
@@ -0,0 +1,13 @@
+const two = "2";
+const oneFource = "1/4";
+
+marinaraSauce =  (cookName) => {
+    console.log(`- Hey ${cookName}! This is my marinara sauce recipe!`);
+    console.log("- Heat a medium-large saucepan over medium heat");
+    console.log(`- Add ${two} tspns of oil and 5 garlic cloves, cook until golden, about ${two} mins`);
+    console.log(`- Add ${oneFource} cup of water, ${two} cans of crushed tomatoes, salt and season with black pepper to taste`);
+    console.log("- Cover the pot, bring to a boil, reduce the heat to medium low , and simmer until the sauce is heated, about 10 mins");
+    console.log(`- Stir in ${oneFource} cup cougly chopped fresh basil,salt and pepper as needed`);
+}
+
+marinaraSauce("Adam");

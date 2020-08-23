495fdd14adc59ae4f935754691b644d33f79a4f1
nastrodamous <andre@andrejdavis.com>
2020-08-23 14:52:54 -0400

    updated for js excercises

---
+++ week1/js101-excercises/app.js
@@ -0,0 +1,86 @@
+// hello = (name) => {
+// if(name) {
+//     console.log(`Hello ${name}`);
+// } else {
+//     console.log("hello world");
+// }
+
+// var outputName = (name) ? `Hello ${name}` : "hello world";
+// console.log(outputName);
+// }
+
+// hello("kobe");
+
+
+// madLib = (string1,string2) => {
+//     return string = `${string1}'s favorite subject in school is ${string2}`
+// }
+
+// madLib("kobe","basketball")
+
+// tipCalculator = (number,serviceLevel) => {
+//     let tip;
+//     if(serviceLevel == "good") {
+//         tip = number * .2
+//     } else if (serviceLevel == "fair") {
+//         tip = number * .15
+//     } else if (serviceLevel == "poor") {
+//         tip = number * .10
+//     } else {
+//         tip = "shit service";
+// }
+// return tip;
+// }
+
+// tipCalculator(100, 'good');
+
+
+// totalAmount = (number,serviceLevel) => {
+    
+//     let total = tipCalculator(number,serviceLevel) + number;
+//     return total;
+// }
+
+// console.log("total function")
+// totalAmount(100,'good');
+
+//  splitAmount = (number,servicelevel,people) => {
+//     let splitCost = totalAmount(number,servicelevel) / people
+//     console.log(splitCost);
+
+// }
+
+// splitAmount(100,'good',5);
+
+// printNumbers = (startNumber,endNumber) =>  {
+
+//     for(let i = startNumber;i <= endNumber;i++) {
+//         console.log(i)
+//     }
+// }
+
+// printNumbers(1,10);
+
+
+// printSquares = (num) => {
+//     for(let i = 0;i <= num;i++ ) {
+//         console.log("*".repeat(num));
+//     }
+// }
+// printSquares(5);
+
+// calculateNum = (num) => {
+//     let str = [];
+//     for(let i = 1; i <= num;i++){
+//         if (num % i == 0) {
+//             str.push(i);
+//         }
+//     }
+//     console.log(str);
+// }
+
+// calculateNum(232);
+
+encodeCipher = () => {
+    
+}

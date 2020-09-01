86a0ad246cae3ff84ac7d120756c7fcbf5473d5f
Andre Davis <andre@Prongs.local>
2020-09-01 18:30:13 -0400

    finished functions

--- week2/arrayMethods.js
+++ week2/arrayMethods.js
@@ -1,92 +1,97 @@
-// var numArray = [-10,-9,-8,-7,-6,0,1,2,3,4,5,6,7];
+// var numArray = [-10,-9,-8,-7,-6,0,1,2,3,4,5,6,7];
 
-// numArray.filter(function (element) {
-//     return element > 0;
-// });
+// numArray.filter(function (element) {
+//     return element > 0;
+// });
 
-// numArray.filter(function (element) {
-//     return element % 2 == 0;
-// });
+// numArray.filter(function (element) {
+//     return element % 2 == 0;
+// });
 
-// numArray.filter(function (element) {
-//     return element * element;
-// });
+// numArray.filter(function (element) {
+//     return element * element;
+// });
 
-// var cities = [
-//     { name: 'Los Angeles', temperature: 60.0},
-//     { name: 'Atlanta', temperature: 52.0 },
-//     { name: 'Detroit', temperature: 48.0 },
-//     { name: 'New York', temperature: 80.0 }
-//   ];
+// var cities = [
+//     { name: 'Los Angeles', temperature: 60.0},
+//     { name: 'Atlanta', temperature: 52.0 },
+//     { name: 'Detroit', temperature: 48.0 },
+//     { name: 'New York', temperature: 80.0 }
+//   ];
 
-//   var newArr = cities.filter(function (element) {
-//     return element.temperature > 70;
-//   });
+//   var newArr = cities.filter(function (element) {
+//     return element.temperature > 70;
+//   });
 
-//    console.log(newArr);
+//    console.log(newArr);
 
-// var people = [
-//     'Dom',
-//     'Lyn',
-//     'Kirk',
-//     'Autumn',
-//     'Trista',
-//     'Jesslyn',
-//     'Kevin',
-//     'John',
-//     'Eli',
-//     'Juan',
-//     'Robert',
-//     'Keyur',
-//     'Jason',
-//     'Che',
-//     'Ben'
-//   ];
+// var people = [
+//     'Dom',
+//     'Lyn',
+//     'Kirk',
+//     'Autumn',
+//     'Trista',
+//     'Jesslyn',
+//     'Kevin',
+//     'John',
+//     'Eli',
+//     'Juan',
+//     'Robert',
+//     'Keyur',
+//     'Jason',
+//     'Che',
+//     'Ben'
+//   ];
 
-//   people.forEach(function (name) {
-//       console.log(`Good Job, ${name}`);
-//   });
+//   people.forEach(function (name) {
+//       console.log(`Good Job, ${name}`);
+//   });
 
+// console.log(people.sort());
 
+// var newArr = people.sort((a,b) => {
+//     return a.length - b.length;
+// })
 
-// console.log(people.sort());
+// console.log(newArr)
 
-// var newArr = people.sort((a,b) => {
-//     return a.length - b.length;
-// })
+// var arr = [
+//     [1, 3, 4],
+//     [2, 4, 6, 8],
+//     [3, 6]
+//   ];
 
-// console.log(newArr)
+//   let sum = (el) => el.reduce((a,b) => a + b);
+//   arr.sort((a,b) => sum(a) - sum(b));
+//   console.log(arr);
 
+// function call3Times(fun) {
+//     fun();
+//     fun();
+//     fun();
+//   }
 
+//   helloWorld = () => {
+//     console.log("Hello World");
+//   }
 
+//   callntimes = (fun,times) => {
+//       for(i = 0; i < times;i++)
+//       fun();
+//   }
 
+//   callntimes(helloWorld,5);
 
-var arr = [
-    [1, 3, 4],
-    [2, 4, 6, 8],
-    [3, 6]
-  ];
+// sumNumbers = (num) =>
+// {
+//     const reducer = (sum, current) => sum + current;
+//     console.log(num.reduce(reducer));
+// }
 
-  var newArr = arr.forEach(function (arrList) {
-    arrList.forEach(function (newArrList){
-        return newArrList.reduce((a,b) => {
-            console.log(a + b);
-            return a + b;
-        })
-    })
-});
+//  sumNumbers([1,2,3]);
 
-console.log(arr.sort((a,b) => {
-    return a > b;
-}));
-
-
-
-
-
-function call3Times(fun) {
-    fun();
-    fun();
-    fun();
-}
-
+acronym = (arr) => {
+        console.log(arr.reduce((current, char) => current + char[0], ""));
+    }
+    
+    acronym(['very', 'important', 'person'])
